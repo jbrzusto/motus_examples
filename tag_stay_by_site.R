c3940ffadc8cdd909a7fe376c493aed280b4b425
john brzustowski <jbrzusto@fastmail.fm>
2016-09-08 12:38:38 +0000

    add comments

--- tag_stay_by_site.R
+++ tag_stay_by_site.R
@@ -1,3 +1,8 @@
+## get the earliest and latest timestamp for each tag at each site
+
 library(dplyr)
+## open a virtual data frame from data file for your project
 x = tbl(src_sqlite("2016_PROJECT_globaltags.sqlite"), "tags")
+
+## group by site and tag, then summarize the time period
 x %>% group_by (site, fullID) %>% summarize (mint = min(ts), maxt = max(ts)) %>% arrange(fullID, site)
